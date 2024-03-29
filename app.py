6dd5af09e5c4c0d7ee86ac388d909c66fb9a37b9
qobulovasror <qobulovasror0@gmail.com>
2024-03-29 07:42:46 +0500

    download subtitle

--- app.py
+++ app.py
@@ -1,27 +1,67 @@
-from flask import Flask
 import os
+import time
 import requests
-from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    ContextTypes,
-    filters,
-    MessageHandler,
-)
-from pytube import YouTube  
+import shutil
+from flask import Flask, request, send_from_directory, render_template 
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import SRTFormatter
-load_dotenv()
 
 app = Flask(__name__)
 
 
+def delete_old_files():
+    folder = 'files/'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
-@app.route('/')
+def get_subtitles(url):
+    try:
+        delete_old_files()
+        if not url.startswith("https://www.youtube.com/") and not url.startswith("https://youtu.be/"):
+            return ("Send me youtube vedio URL", 401)
+        r = requests.get(url)
+        if "Video unavailable" in r.text:
+            return ("URL is invalid", 401)
+
+        #downlod subtitle
+        transcript = YouTubeTranscriptApi.get_transcript(url.split("=")[1])
+        formatter = SRTFormatter()
+        caption = formatter.format_transcript(transcript)
+        with open(f'files/vedio.srt', 'w') as the_file:
+            the_file.write(caption)
+        subtitle_file = "files/vedio.srt"
+        return (subtitle_file, 200)
+    except Exception as ex: 
+        return (f"Server error: Somthing went wrong:  {ex}", 501)
+
+
+@app.route('/', methods=["POST", "GET"])
 def home():
-    return  "Hello, World!"
+    if request.method == 'POST':
+        url = request.form['yturl']
+        res_data = get_subtitles(url)
+        print(res_data)
+        if res_data[1]==200:
+            return render_template('index.html', url=res_data[0])
+        else:
+            return render_template('index.html', error=res_data[0], code=res_data[1])
+    else:
+        return render_template('index.html')
+
+
+@app.route('/download/<path:path>',methods = ['GET','POST'])
+def get_files(path):
+    try:
+        return send_from_directory("files", path, as_attachment=True)
+    except FileNotFoundError:
+        return render_template('index.html', error=f"File Not Found : {path}"), 404
 
 if __name__=="__main__":
     app.run(port=5200, debug=True)
